2f1cdeb92bc98b4551e59792ec3febbb0ae1e924
opethrocks <lebleu131@icloud.com>
2021-01-28 09:58:15 -0600

    build issues

--- server/app.js
+++ server/app.js
@@ -53,9 +53,9 @@ app.use('/api', require('./routes'));
 
 // //determine environment
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
