118f62f5f9727983c306bc488c7978ebde45e0f3
opethrocks <lebleu131@icloud.com>
2021-01-29 07:27:26 -0600

    attempt to fix server not available

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,5 +1,7 @@
 module.exports = {
-  MongoURI: process.env.MONGO_URI,
+  MongoURI:
+    'mongodb+srv://mlebleu:Godisgood1@cluster0.mu8u9.mongodb.net/users?retryWrites=true&w=majority',
   jwtAccessKey: process.env.ACCESS_KEY,
-  jwtRefreshKey: process.env.REFRESH_KEY
+  jwtRefreshKey:
+    'e3 71 6f bf de 69 d8 9b 9f dd 6c 03 a7 b3 9c 27 e5 6c ab 82 3d fb e5 5f b0 d3 07 d8 bb 13'
 };
