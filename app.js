12b0f66f1588b7189ca1528bcd3ead05568314be
opethrocks <lebleu131@icloud.com>
2021-02-03 06:40:53 -0600

    broadcast messages to all clients but self

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ const wss = new WebSocket.Server({ noServer: true });
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(data) {
     wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
         client.send(data);
       }
     });
