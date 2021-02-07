c951a8073144fe9a693373e0c86604c593b45c4d
opethrocks <lebleu131@icloud.com>
2021-02-07 09:58:56 -0600

    improve websocket connection

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ const uri =
 mongoose
   .connect(uri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => console.log('MongoDB connected...'))
   .catch((err) => console.log(err));
@@ -49,6 +49,49 @@ if (process.env.NODE_ENV === 'production') {
 //Port
 const port = process.env.PORT || 5000;
 
+const wss = new WebSocket.Server({ noServer: true });
+
+const heartbeat = (ws) => {
+  ws.isAlive = true;
+};
+
+const ping = (ws) => {
+  ws.ping();
+};
+
+wss.on('connection', function connection(ws) {
+  ws.isAlive = true;
+  ws.on('pong', () => {
+    heartbeat(ws);
+  });
+
+  const interval = setInterval(() => {
+    wss.clients.forEach((ws) => {
+      if (ws.isAlive === false) {
+        return ws.terminate();
+      }
+      ws.isAlive = false;
+      ws.ping(() => {
+        ping(ws);
+      });
+    });
+  }, 30000);
+
+  ws.on('message', function incoming(data) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data);
+      }
+    });
+  });
+
+  ws.on('close', function close() {
+    ws.terminate();
+    clearInterval(interval);
+    wss;
+  });
+});
+
 const server = app.listen(port, () =>
   console.log(`Server running on port ${port}`)
 );
@@ -57,44 +100,4 @@ server.on('upgrade', (request, socket, head) => {
   wss.handleUpgrade(request, socket, head, (socket) => {
     wss.emit('connection', socket, request);
   });
-});
-
-//Web socket server
-const wss = new WebSocket.Server({ noServer: true });
-
-//Broadcast messages to all connected clients
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
-  });
-});
-
-//WebSocket Ping/Pong
-
-function noop() {}
-
-function heartbeat() {
-  this.isAlive = true;
-}
-
-wss.on('connection', function connection(ws) {
-  ws.isAlive = true;
-  ws.on('pong', heartbeat);
-});
-
-const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
-
-    ws.isAlive = false;
-    ws.ping(noop);
-  });
-}, 30000);
-
-wss.on('close', function close() {
-  clearInterval(interval);
 });
